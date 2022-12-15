369feb076d3ca5e45752e611c099fef886d3715a
Russell Slater <russellslater@gmail.com>
2022-12-15 00:45:13 -0500

    Added Min / Max utility functions for ints

--- internal/util/util.go
+++ internal/util/util.go
@@ -68,3 +68,17 @@ func MustAtoi(s string) int {
 	}
 	return i
 }
+
+func Max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func Min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
