28825a68ea5a02dd7977a48bdaef70a1756efca9
Russell Slater <russellslater@gmail.com>
2022-12-27 08:01:06 -0500

    Added Turn and Relative methods to Direction

--- internal/twod/direction.go
+++ internal/twod/direction.go
@@ -23,3 +23,55 @@ func (d Direction) String() string {
 		return "Unknown"
 	}
 }
+
+func (d Direction) Relative() RelativeDirection {
+	switch d {
+	case North:
+		return Up
+	case South:
+		return Down
+	case West:
+		return Left
+	case East:
+		return Right
+	default:
+		return 0
+	}
+}
+
+func (d Direction) Turn(turn RelativeDirection) Direction {
+	switch turn {
+	case Left:
+		switch d {
+		case North:
+			return West
+		case South:
+			return East
+		case West:
+			return South
+		case East:
+			return North
+		}
+	case Right:
+		switch d {
+		case North:
+			return East
+		case South:
+			return West
+		case West:
+			return North
+		case East:
+			return South
+		}
+	}
+	return d
+}
+
+type RelativeDirection int
+
+const (
+	Up RelativeDirection = iota
+	Down
+	Left
+	Right
+)
